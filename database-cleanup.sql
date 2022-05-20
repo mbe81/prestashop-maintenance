35efdfd8d6f3f61b66834687cd0609e30a7950ae
Marcel van den Berg <34321907+mbe81@users.noreply.github.com>
2022-05-20 16:37:19 +0200

    Update database-cleanup.sql

--- database-cleanup.sql
+++ database-cleanup.sql
@@ -11,7 +11,7 @@ The script takes care of cleaning the following:
 
 */
 
-
+-- carts
 delete from ps_cart 
  where id_cart not in (select id_cart from ps_orders)
    and date_add < subdate(curdate(), 365);
@@ -19,6 +19,7 @@ delete from ps_cart
 delete from ps_cart_product 
  where id_cart not in (select id_cart from ps_cart);
 
+-- connections
 delete from ps_connections
  where date_add < subdate(curdate(), 365);
 
@@ -28,13 +29,16 @@ delete from ps_connections_page
 delete from ps_connections_source
  where id_connections not in (select id_connections from ps_connections);
 
+-- guest
 delete from ps_guest 
  where id_guest not in (select id_guest from ps_cart)
    and id_guest not in (select id_guest from ps_connections)
    and id_guest not in (select id_guest from ps_product_comment);
 
+-- pagenotfound
 delete from ps_pagenotfound
  where date_add < subdate(curdate(), 365);
 
+-- statsssearch
 delete from ps_statssearch
  where date_add < subdate(curdate(), 365);
